bf7bbe0ce0ccdee3245a43a43ef767b9d38da598
nikhilk1699 <109533285+nikhilk1699@users.noreply.github.com>
2023-11-06 15:25:28 +0530

    Update package.sh

--- package.sh
+++ package.sh
@@ -6,12 +6,6 @@ show_message() {
   echo "-------------------------------------------------------------"
 }
 
-show_message "Check if the script is being run as the root user"
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script as the root user."
-    exit 1
-fi
-
 show_message "Update package lists and install necessary packages"
 sudo apt update
 sudo apt install apache2 -y
@@ -27,29 +21,90 @@ show_message "Restart Apache"
 sudo systemctl restart apache2
 
 show_message "Install MariaDB and secure the installation"
-sudo apt install mariadb-server mariadb-client -y
+sudo  apt install mariadb-server mariadb-client -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
-sudo mysql_secure_installation 
+
+show_message "Configure MariaDB"
+sudo mysql -e  <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'snipe1';
+DELETE FROM mysql.user WHERE User='';
+FLUSH PRIVILEGES;
+EOF
 
 show_message "Install PHP and necessary extensions"
 sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath -y
 
 show_message "Additional PHP extensions"
-sudo apt install php php-bcmath php-bz2 php-intl php-gd php-mbstring php-mysql php-zip php-opcache php-pdo php-calendar php-ctype php-exif php-ffi php-fileinfo php-ftp php-iconv php-intl php-json php-mysqli php-phar php-posix php-readline php-shmop php-sockets php-sysvmsg php-sysvsem php-sysvshm php-tokenizer php-curl php-ldap -y
+sudo apt install php-bz2 php-intl php-ffi php-fileinfo php-ftp php-iconv php-json php-mysqli php-phar php-posix php-readline php-shmop php-sockets php-sysvmsg php-sysvsem php-sysvshm php-tokenizer php-curl php-ldap -y
 
 show_message "Install Composer"
-sudo curl -sS https://getcomposer.org/installer | php
+curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
 show_message "Create a MySQL database and user for Snipe-IT"
-
-mysql -u root -p <<EOF
+sudo mysql <<EOF
 CREATE DATABASE snipeitdb;
 CREATE USER snipeituser@localhost IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON snipeitdb.* TO snipeituser@localhost;
 FLUSH PRIVILEGES;
 EOF
 
-echo "packages installation setup complete!"
+GIT_USERNAME="nikhilk1669"
+GIT_PASSWORD="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"
+REPO_URL="https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/PearlThoughtsInternship/snipe-it.git"
+
+show_message "Clone the repository and set up Snipe-IT"
+sudo chown -R linuxusr:linuxusr /var/www/
+cd /var/www/ || exit 1
+git clone $REPO_URL
+cd snipe-it || exit 1
+cp .env.example .env
+cat <<EOL > .env
+APP_DEBUG=false
+APP_KEY=
+APP_URL=
+DB_DATABASE=snipeitdb
+DB_USERNAME=snipeituser
+DB_PASSWORD=admin
+EOL
+
+show_message "Set permissions and install dependencies"
+sudo chown -R www-data:www-data /var/www/snipe-it
+sudo chmod -R 755 /var/www/snipe-it
+yes | sudo composer update --no-plugins --no-scripts
+yes | sudo composer install --no-dev --prefer-source --no-plugins --no-scripts
+yes | php artisan key:generate
+
+show_message "Configure Apache"
+sudo a2dissite 000-default.conf
+
+show_message "Define the content to be added to the snipe-it.conf file"
+content="<VirtualHost *:80>
+    ServerName snipe-it.syncbricks.com
+    DocumentRoot /var/www/snipe-it/public
+    <Directory /var/www/snipe-it/public>
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        Order allow,deny
+        allow from all
+    </Directory>
+</VirtualHost>"
+
+show_message "Add the content to the snipe-it.conf file"
+echo "$content" | sudo tee /etc/apache2/sites-available/snipe-it.conf > /dev/null
+
+show_message "Restart Apache for changes to take effect"
+sudo systemctl restart apache2
+
+sudo a2ensite snipe-it.conf
+
+show_message "Adjust permissions and restart Apache"
+sudo chown -R www-data:www-data /var/www/snipe-it/storage
+sudo chmod -R 755 /var/www/snipe-it/storage
+sudo systemctl restart apache2
+
+show_message "Snipe-IT setup complete!"
+
+
 
